7966f85aa81c727892dc5ce1e1a13a3a5d58bb66
Arvindynokia <arvindec8791@gmail.com>
2025-07-19 09:39:20 -0700

    module add

--- modules/azurerm_virtual_machine/data.tf
+++ modules/azurerm_virtual_machine/data.tf
@@ -1,8 +1,4 @@
-data "azurerm_public_ip" "kuch_bhi_ip" {
-  name                = var.frontend_ip_name
-  resource_group_name = var.resource_group_name
-}
-
+data "azurerm_public_ pic
 data "azurerm_subnet" "frontend_subnet" {
   name                 = var.frontend_subnet_name
   virtual_network_name = var.vnet_name
