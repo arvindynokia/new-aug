b2ee08ebb9388c9a8462e648054b88bc01b4b327
arvindynokia <arvindec8791@gmail.com>
2025-08-11 22:41:46 -0700

    Update main.tf

--- modules/azurerm_virtual_machine/main.tf
+++ modules/azurerm_virtual_machine/main.tf
@@ -4,21 +4,22 @@ resource "azurerm_network_interface" "nic" {
   resource_group_name = var.resource_group_name
 
   ip_configuration {
-    public_ip_address_id          = data.azurerm_public_ip.kuch_bhi_ip.id
     name                          = "internal"
     subnet_id                     = data.azurerm_subnet.frontend_subnet.id
+    public_ip_address_id          = data.azurerm_public_ip.kuch_bhi_ip.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                            = var.vm_name
-  resource_group_name             = var.resource_group_name
-  location                        = var.location
-  size                            = var.vm_size
-  admin_username                  = data.azurerm_key_vault_secret.vm-username.value
-  admin_password                  = data.azurerm_key_vault_secret.vm-password.value
+  name                             = var.vm_name
+  resource_group_name              = var.resource_group_name
+  location                         = var.location
+  size                             = var.vm_size
+  admin_username                   = data.azurerm_key_vault_secret.vm-username.value
+  admin_password                   = data.azurerm_key_vault_secret.vm-password.value
   disable_password_authentication = false
+
   network_interface_ids = [
     azurerm_network_interface.nic.id,
   ]
@@ -29,20 +30,19 @@ resource "azurerm_linux_virtual_machine" "vm" {
   }
 
   source_image_reference {
-    publisher = var.image_publisher # Published ID from Azure Marketplace
-    offer     = var.image_offer     # Product ID from Azure Marketplace
-    sku       = var.image_sku       # Plan ID from Azure Marketplace
-    version   = var.image_version   # Version of the image
+    publisher = var.image_publisher
+    offer     = var.image_offer
+    sku       = var.image_sku
+    version   = var.image_version
   }
 
   custom_data = base64encode(<<-EOF
-    #!/bin/bash
-    apt-get update
-    apt-get install -y nginx
-    systemctl enable nginx
-    systemctl start nginx
-  EOF
+#!/bin/bash
+apt-get update
+apt-get install -y nginx
+systemctl enable nginx
+systemctl start nginx
+EOF
   )
-
 }
 
